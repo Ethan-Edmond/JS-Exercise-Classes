5f88e40bc2fe1eabe4b7750cb4789d0d39146567
Ethan-Edmond <eedmond@uccs.edu>
2021-04-22 13:16:16 -0600

    Finished task 4

--- index.js
+++ index.js
@@ -144,8 +144,8 @@ const me = new Lambdasian({
   location: "Colorado Springs"
 });
 
-console.log(me);
-console.log(me.speak());
+// console.log(me);
+// console.log(me.speak());
 
   /*
     TASK 4
@@ -161,9 +161,35 @@ console.log(me.speak());
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
-
+ class Instructor extends Lambdasian{
+   constructor(argObj){
+     super(argObj);
+     this.specialty = argObj.specialty;
+     this.favLanguage = argObj.favLanguage;
+     this.catchPhrase = argObj.catchPhrase;
+   }
+   demo(subject){
+     return `Today we are learning about ${subject}`;
+   }
+   grade(student,subject){
+     return `${student.name} receives a perfect score on ${subject}`;
+   }
  }
+
+const brit = new Instructor({
+  name: "Brit",
+  age: 31, // I think she said 31 or 32
+  location: "Toronto",
+  specialty: "JS",
+  favLanguage: "JS",
+  catchPhrase: "A real one never dies"
+});
+
+// console.log(brit);
+// console.log(brit.demo("JsSsSs"));
+// console.log(brit.speak());
+// console.log(brit.grade(me, "JSSSSssss..."));
+
   /*
     TASK 5
       - Write a Student class extending Lambdasian.
